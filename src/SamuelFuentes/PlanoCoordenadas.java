5bb467225919e2d2ec8ce32ec14c55328c65c20b
SamuGITC <samuel.fuentes@epn.edu.ec>
2026-05-19 01:07:01 -0500

    Save user modifications to PlanoCoordenadas.java

--- src/SamuelFuentes/PlanoCoordenadas.java
+++ src/SamuelFuentes/PlanoCoordenadas.java
@@ -8,13 +8,14 @@ public class PlanoCoordenadas {
 
         int[] y = new int[nombre.length()];
 
+        // y = 2x
         for (int x = 0; x < nombre.length(); x++) {
-            y[x] = x + 1;
+            y[x] = 2 * x;
         }
 
         int maxY = y[y.length - 1];
 
-        for (int fila = maxY; fila > 0; fila--) {
+        for (int fila = maxY; fila >= 0; fila--) {
 
             System.out.printf("%2d | ", fila);
 
@@ -30,12 +31,12 @@ public class PlanoCoordenadas {
             System.out.println();
         }
 
-        System.out.println("0 |________________");
+        System.out.println("   __________________");
     }
 
     public static void main(String[] args) {
 
-        System.out.println("Grupo 3");
+        System.out.println("Grupo 1");
         System.out.println("Integrante: Fuentes.Samuel");
 
         g1_planoCoordenadas();
